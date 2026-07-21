8756945e1381b13b866197851e584c69fb0e9686
Vlade <vladecassia@gmail.com>
2026-07-21 17:36:08 -0300

    calcular idade

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿//Crie um método do tipo void chamado MostrarIdade
 // deve receber o ano de nascimento (int), 
 // calcular a idade baseada no ano atual e exibir no console.writeline
+
+Console.WriteLine("Digite o seu ano de nascimento: ");
+int anoDigitado = int.Parse(Console.ReadLine());
+
+MostrarIdade(anoDigitado);
+
+void MostrarIdade(int anoNascimento)
+{
+    int idade = 2026 - anoNascimento;
+
+    Console.WriteLine($"Sua idade em 2026 será: {idade} anos. ");
+}
